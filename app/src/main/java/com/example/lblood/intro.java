208ddf7f1eb1dd7335929e88a4cf5efe1695a772
mahdinehme <mahdinehme23@gmail.com>
2020-01-12 15:37:11 +0200

    onclick:form and search activity will start

--- app/src/main/java/com/example/lblood/intro.java
+++ app/src/main/java/com/example/lblood/intro.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class intro extends AppCompatActivity {
 
@@ -14,17 +15,26 @@ public class intro extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        ImageView donner = (ImageView) findViewById(R.id.imageView2);
+        donner.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(intro.this, fillForm.class);
 
+                intro.this.startActivity(myIntent);
+            }
+        });
 
+        ImageView donate = (ImageView) findViewById(R.id.imageView1);
+        donate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent2 = new Intent(intro.this, search.class);
 
+                intro.this.startActivity(myIntent2);
+            }
+        });
 
 
     }
 
-    public void opendonner(View view) {
-        Intent newInt=new Intent(this,fillForm.class);
 
-
-
-    }
 }
