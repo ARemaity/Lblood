eab45b3feac97f953d87879e10f3a1a022627fd2
mahdinehme <mahdinehme23@gmail.com>
2020-01-12 18:22:26 +0200

    Search and single Result

---
+++ app/src/main/java/com/example/lblood/requestCall.java
@@ -0,0 +1,14 @@
+package com.example.lblood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class requestCall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_call);
+    }
+}
